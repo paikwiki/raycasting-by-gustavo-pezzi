a242d8a32f5652bf2c3c10b22ab41312eda6f54d
paikwiki <paikwiki@gmail.com>
2020-09-11 14:46:55 +0900

    Done "Defining the 2D Map Grid"

--- codes/raycast.js
+++ codes/raycast.js
@@ -1,5 +1,44 @@
+const TILE_SIZE = 32;
+const MAP_NUM_ROWS = 11;
+const MAP_NUM_COLS = 15;
+
+const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
+const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+
+class Map {
+	constructor() {
+		this.grid = [
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+			[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		];
+	}
+	render() {
+		for (var i = 0; i < MAP_NUM_ROWS; i++) {
+			for (var j = 0; j < MAP_NUM_COLS; j++) {
+				var tileX = j * TILE_SIZE;
+				var tileY = i * TILE_SIZE;
+				var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
+				stroke("#222");
+				fill(tileColor);
+				rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+			}
+		}
+	}
+}
+
+var grid = new Map();
+
 function setup() {
-	// TODO: initialize all objects
+	createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 function update() {
@@ -8,5 +47,5 @@ function update() {
 
 function draw() {
 	update();
-	// TODO: draw all objects frame by frame
+	grid.render();
 }
