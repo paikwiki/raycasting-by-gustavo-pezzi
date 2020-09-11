49ef9b31ba6e7e58e8decedd8d671d1f6ed0d3f3
paikwiki <paikwiki@gmail.com>
2020-09-11 14:30:36 +0900

    Create skeleton code

---
+++ codes/raycast.js
@@ -0,0 +1,12 @@
+function setup() {
+	// TODO: initialize all objects
+}
+
+function update() {
+	// TODO: update all game objects before we render the next frame
+}
+
+function draw() {
+	update();
+	// TODO: draw all objects frame by frame
+}
